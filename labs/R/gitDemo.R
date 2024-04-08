302651a7d6ade3f95dd94d0d3bb7102bb3d51448
unknown <mega@mysaga.uu.me>
2024-04-08 14:24:11 -0700

    Lab1 with hello world, oneplus and vectorized oneplus

--- labs/R/gitDemo.R
+++ labs/R/gitDemo.R
@@ -1,1 +1,9 @@
 print("hello world")
+library(Rcpp)
+cppFunction('int oneplus( int x ) {
+    x++;
+    return x;
+          }') 
+vecOneplus <- Vectorize(oneplus)
+vecOneplus( 1:5 )
+all.equal( vecOneplus( 1:5 ), 2:6)
